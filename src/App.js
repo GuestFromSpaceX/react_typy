fdb28fe0407f0295d6fed455e0d37628caf96004
GuestFromSpaceX <gfromsx@gmail.com>
2023-06-19 23:19:16 +0300

    1 day and

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
 import React, { useState } from 'react';
 import './styles/App.css';
-import './media/logo.svg';
+import logo from './media/logo.svg';
+import hero from './media/skeleton-hero.png';
+import enemy from './media/skeleton.png';
 import HP from './components/HP';
+import MainInput from './components/MainInput';
 
 function App() {
 
   return (
     <div className="App">
+
       <header class="bg-[#998414] flex flex-row flex-wrap justify-around items-center">
-    <div name='logo' class="">
-        <img src='./media/logo.svg' />
-    </div>
-    <div name='play-menu-button' class="bg-[#ff6fff]/50">
-        <button class="">Play</button>
-    </div>
-    <div name='stats-menu-button' class="bg-[#ff5fff]/50">
-        <button class="">Player Stats</button>
-    </div>
-    <div name='log-in-button' class="bg-[#ff3fff]/50">
-        <button class="">Log in</button>
-    </div>       
-  </header>
-  <main class="bg-[#ff1fff]/50 flex flex-col items-center">
-    <div name='top-main' class="bg-[#ff2fff]/50 flex flex-row justify-center">
-       <div name='player' class="bg-[#ff3fff]/50 flex flex-col items-center">
-        <div name='player-pic' class="bg-red-100">
-            <img src='../media/skeleton-hero.png' alt='Изображение игрока'/>
+        <div name='logo' class="">
+            <img src={logo} alt='Логотип'/>
         </div>
-        <div name='player-stats' class="bg-red-200">
-            НР
+        <div name='play-menu-button' class="bg-[#ff6fff]/50">
+            <button class="">Play</button>
         </div>
-       </div> 
-       <div name='log' class="bg-yellow-300">
-            Раунд 1
-       </div> 
-       <div name='enemy' class="bg-[#ff7fff]/50 flex flex-col items-center">
-        <div name='enemy-pic' class="bg-blue-100">
-            <img src='../media/skeleton.png' alt='Изображение противника'/>
+        <div name='stats-menu-button' class="bg-[#ff5fff]/50">
+            <button class="">Player Stats</button>
         </div>
-        <div name='enemy-stats' class="bg-blue-300">
-            НР-enemy
+        <div name='log-in-button' class="bg-[#ff3fff]/50">
+            <button class="">Log in</button>
+        </div>       
+      </header>
+
+      <main class="bg-[#ff1fff]/50 flex flex-col items-center">
+        <div name='top-main' class="bg-[#ff2fff]/50 flex flex-row justify-center">
+          <div name='player' class="bg-[#ff3fff]/50 flex flex-col items-center">
+            <div name='player-pic' class="bg-red-100">
+              <img src={hero} alt='Изображение игрока'/>
+            </div>
+            <div name='player-stats' class="bg-red-200">
+              <HP/>
+            </div>
+          </div> 
+          <div name='log' class="bg-yellow-300">
+                Раунд 1
+          </div> 
+          <div name='enemy' class="bg-[#ff7fff]/50 flex flex-col items-center">
+            <div name='enemy-pic' class="bg-blue-100">
+              <img src={enemy} alt='Изображение противника'/>
+            </div>
+            <div name='enemy-stats' class="bg-blue-300">
+              <HP/>
+            </div>
+          </div>     
         </div>
-       </div>     
-    </div>
-    <div name='main-input' class="bg-green-100">
-        Место ВВОДА
-    </div>
-    <div name='main-keyboard' class="bg-green-200">
-        КЛАВИАТУРА
-    </div>    
-  </main>
-  <footer class="bg-blue-200">
+        <div name='main-input-block' class="bg-green-100">
+          <MainInput/>
+        </div>
+        <div name='main-keyboard' class="bg-green-200">
+          КЛАВИАТУРА
+        </div>    
+      </main>
+
+      <footer class="bg-blue-200">
         Footer
-  </footer>
+      </footer>
     </div>
   );
 }
---
+++ src/components/MainInput.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const MainInput = function () {
+
+
+    return (
+        <>
+            <input class='' placeholder="Введи пока что это"></input>
+        </>
+    )
+}
+
+export default MainInput;
