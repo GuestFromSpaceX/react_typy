355f609d2dc993017654eaea556cbb50d9fedb22
GuestFromSpaceX <gfromsx@gmail.com>
2023-06-20 15:01:05 +0300

    fix

--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -1,13 +1,27 @@
 import React, {useState} from "react";
 
-const MainInput = function () {
+export const Round = function () {
+    const words = ['apple', 'banana', 'orange', 'grape', 'melon'];
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const randomWord = words[randomIndex];
 
+    const [count, setCount] = useState(1)
+    const [ranWord, setRanWord] = useState(randomWord)
 
     return (
         <>
-            <input class='' placeholder="Введи пока что это"></input>
+            <h1 className="">Раунд {count}</h1>
+            <h2 className="">Для атаки введи {ranWord}</h2>
         </>
     )
 }
 
-export default MainInput;
+export const MainInput = function ({ ranWord }) {
+
+
+    return (
+        <>
+            <input className='' placeholder={`Введи "${ranWord}"`}></input>
+        </>
+    )
+}
